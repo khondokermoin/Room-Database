c128ecea65eac2c53021a7887a1be777eaf48555
khondokermoin <mouin2k19@gmail.com>
2024-06-25 15:38:47 +0600

    show data

---
+++ app/src/main/java/com/example/roomdatabase/ShowData.kt
@@ -0,0 +1,15 @@
+package com.example.roomdatabase
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ShowData : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_data)
+
+    }
+}
